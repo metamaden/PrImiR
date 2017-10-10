609590fd7cb6efad09e8e6a7e1b377f47c06acb2
smaden <metamaden@users.noreply.github.com>
2017-10-10 11:32:51 -0700

    Update mirna-hairpin_chrcoor-miR-cpg-annotations.R

--- scripts/mirna-hairpin_chrcoor-miR-cpg-annotations.R
+++ scripts/mirna-hairpin_chrcoor-miR-cpg-annotations.R
@@ -57,26 +57,8 @@ for(i in 1:nrow(hsacoor)){
 
 save(hsacoor,file="mirna-hairpin_chrcoor-df.rda")
 
-#================================
-# take overlapping CpGs from BeadChip array annotations
-
-#dim(getAnnotation(rg.epic.sub))
-#epicanno <- as.data.frame(getAnnotation(rg.epic.sub))
-#epic450.anno <- epicanno[!rownames(epicanno) %in% rownames(anno.hm450),]
-#r1 <- anno.hm450[,c("chr","pos","strand","Name","Islands_Name","Relation_to_Island","UCSC_RefGene_Name","UCSC_RefGene_Group")]
-#r2 <- epic450.anno[,c("chr","pos","strand","Name","Islands_Name","Relation_to_Island","UCSC_RefGene_Name","UCSC_RefGene_Group")]
-
-#epic450.anno <- rbind(r1,r2)
-#head(epic450.anno)
-
-#save(epic450.anno,file="CpG_BeadChip_anno_EPIC450.rda")
-
-
 #============================================================
 # take intersect of granges objects, CpG anno and hsacoor
-
-#
-
 
 # new hsa miRNA df, every row is a unique range
 a <- data.frame(mirna=c(hsacoor[1:529,1],hsacoor[530,1],hsacoor[530,1],hsacoor[531:nrow(hsacoor),1]),
@@ -86,41 +68,60 @@ a <- data.frame(mirna=c(hsacoor[1:529,1],hsacoor[530,1],hsacoor[530,1],hsacoor[5
                 strand=c(hsacoor[1:529,5],hsacoor[530,5],hsacoor[530,5],hsacoor[531:nrow(hsacoor),5]),
                 stringsAsFactors=FALSE);
 
-# promoter df
-ap <- a
-pend <- ifelse(a$strand=="+",a$start-1000,a$start+1000) # designate promoter region ranges w.in 1KB relative to start, using strandedness
-for(i in 1:nrow(ap)){
-  pcoori <- c(ap$start[i],pend[i])
-  ap$start[i] <- min(pcoori); ap$end[i] <- max(pcoori)
-  message(round(100*(i/nrow(ap)),3),"%")
+a$start.promoter <- NA; a$end.promoter <- NA
+
+pend <- ifelse(a$strand=="+",a$start-1500,a$start+1500) # designate promoter region ranges w.in 1.5KB relative to start, using strandedness
+
+for(i in 1:nrow(a)){
+  pcoori <- c(a$start[i],pend[i])
+  a$start.promoter[i] <- min(pcoori); a$end.promoter[i] <- max(pcoori)
+  message(round(100*(i/nrow(a)),3),"%")
 }; 
 
 # GRanges object for hsa miRNA hairpin seq's
 #grhsa <- makeGRangesFromDataFrame(a,keep.extra.columns = TRUE)
-grhsap <- makeGRangesFromDataFrame(ap,keep.extra.columns = TRUE)
+x <- a; colnames(x) <- c(colnames(x)[1:2],"start.mirna","end.mirna",colnames(x)[5],"start","end")
+grhsap <- makeGRangesFromDataFrame(x,keep.extra.columns = TRUE)
+
 # GRanges object for CpG annotations from EPIC and HM450 arrays
 cpg <- epic450.anno; cpg$start <- cpg$pos; cpg$end <- ifelse(cpg$strand=="+",cpg$pos+1,cpg$pos-1)
 grcpg <- makeGRangesFromDataFrame(cpg,keep.extra.columns = TRUE)
 
-#ai <- intersect(grhsa,grcpg,ignore.strand=TRUE)
-#cpg.mirna <- grcpg[start(grcpg) %in% start(ai)]
+# GRanges object for only 450k CpGs
+cpg450 <- r1; cpg450$start <- cpg450$pos; cpg450$end <- ifelse(cpg450$strand=="+",cpg450$pos+1,cpg450$pos-1)
+grcpg450 <- makeGRangesFromDataFrame(cpg450,keep.extra.columns = TRUE)
 
-api <- findOverlaps(grcpg,grhsap,ignore.strand=TRUE)
-cpg.mirnaprom <- cpg[cpg$Name %in% mcols(grcpg[unique(queryHits(api))])$Name,]
-mpcpg <- ap[ap$mirna %in% mcols(grhsap[unique(subjectHits(api))])$mirna,]
-colnames(mpcpg) <- c("mirna","chr","start.prom","end.prom","strand")
-xa <- a[a$mirna %in% mpcpg$mirna,]; identical(xa$mirna,mpcpg$mirna)
-mpcpg$start.mirna <- xa$start; mpcpg$end.mirna <- xa$end
+# GRanges object for only EPIC CpGs
+cpgepic <- r3; cpgepic$start <- cpgepic$pos; cpgepic$end <- ifelse(cpgepic$strand=="+",cpgepic$pos+1,cpgepic$pos-1)
+grcpgepic <- makeGRangesFromDataFrame(cpgepic,keep.extra.columns = TRUE)
 
-xcpg <- cpg.mirnaprom; xmirna <- mpcpg
-xmirna$cpg <- NA
+mcpg <- a
+
+xmirna <- mpcpg
+xmirna$cpg <- NA; xmirna$cpg450 <- NA; xmirna$cpgepic <- NA
 for(i in 1:nrow(xmirna)){
+  # All BeadChip overlapping CpGs
   which <- queryHits(findOverlaps(grcpg,
                                   grhsap[mcols(grhsap)$mirna==xmirna$mirna[i]],
                                   ignore.strand=TRUE))
   
   xmirna$cpg[i] <- paste(mcols(grcpg[which])$Name,
                          collapse=";")
+  
+  # HM450 overlapping CpGs
+  which450 <- queryHits(findOverlaps(grcpg450,
+                                  grhsap[mcols(grhsap)$mirna==xmirna$mirna[i]],
+                                  ignore.strand=TRUE))
+  xmirna$cpg450[i] <- paste(mcols(grcpg450[which450])$Name,
+                         collapse=";")
+  
+  # EPIC overlapping CpGS
+  whichepic <- queryHits(findOverlaps(grcpgepic,
+                                     grhsap[mcols(grhsap)$mirna==xmirna$mirna[i]],
+                                     ignore.strand=TRUE))
+  xmirna$cpgepic[i] <- paste(mcols(grcpgepic[whichepic])$Name,
+                            collapse=";")
+  
   message(round(100*(i/nrow(xmirna)),3),"%")
 }
 
@@ -130,4 +131,6 @@ for(i in 1:nrow(xmirna)){
   message(round(100*(i/nrow(xmirna)),3),"%")
 }
 
-save(xmirna,file="mirna-anno_CpG-miR.rda")
+save(xmirna,file="mirna-anno_CpG-450-epic-miR.rda")
+
+###
